0de1c3a3b964d3ff4228f0ec7138468f84fefd52
neilchowCS <neilxchow@gmail.com>
2025-05-07 19:29:57 -0700

    add dbconnection class

--- backend/application/src/main/java/com/example/App.java
+++ backend/application/src/main/java/com/example/App.java
@@ -8,40 +8,9 @@ public class App
 {
     public static void main( String[] args )
     {
-        System.out.println( "Hello World!" );
-
-        String url = "jdbc:mysql://localhost:3306/gamedb";
-        String user = "user";
-        String password = "password";
-
-        try (Connection conn = DriverManager.getConnection(url, user, password)) {
-            System.out.println("Connected to MySQL database!");
-            /*
-            String sql = "INSERT INTO player (username) VALUES ('playerOne')";
-            PreparedStatement stmt = conn.prepareStatement(sql);
-            stmt.executeUpdate();
-            */
-            Statement s = conn.createStatement();
-            ResultSet rs = s.executeQuery("SELECT * FROM player");
-
-            System.out.println("player_id | username   | player_level | player_rank");
-            System.out.println("----------------------------------------");
-
-            while (rs.next()) {
-                int id = rs.getInt("player_id");
-                String username = rs.getString("username");
-                int level = rs.getInt("player_level");
-                String rank = rs.getString("player_rank");
-
-                System.out.printf("%9d | %-10s | %5d | %-10s%n", id, username, level, rank);
-            }
-
-        } catch (SQLException e) {
-            System.out.println("Connection failed:");
-            e.printStackTrace();
-        }
-
-
+        DBConnection dbc = new DBConnection();
+        dbc.InsertPlayer("playerTwo");
+        dbc.PrintPlayers();
     }
 
 }
---
+++ backend/application/src/main/java/com/example/DBConnection.java
@@ -0,0 +1,45 @@
+package application.src.main.java.com.example;
+import java.sql.*;
+
+public class DBConnection {
+    String url = "jdbc:mysql://localhost:3306/gamedb";
+    String user = "user";
+    String password = "password";
+
+    public DBConnection(){
+
+    }
+
+    public void InsertPlayer(String Username){
+        try (Connection conn = DriverManager.getConnection(url, user, password)) {
+            String sql = "INSERT INTO player (username) VALUES (?)";
+            PreparedStatement stmt = conn.prepareStatement(sql);
+            stmt.setString(1, Username);
+            stmt.executeUpdate();
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+    }
+
+    public void PrintPlayers(){
+        try (Connection conn = DriverManager.getConnection(url, user, password)) {
+            Statement s = conn.createStatement();
+            ResultSet rs = s.executeQuery("SELECT * FROM player");
+
+            System.out.println("player_id | username   | player_level | player_rank");
+            System.out.println("----------------------------------------");
+
+            while (rs.next()) {
+                int id = rs.getInt("player_id");
+                String username = rs.getString("username");
+                int level = rs.getInt("player_level");
+                String rank = rs.getString("player_rank");
+
+                System.out.printf("%9d | %-10s | %5d | %-10s%n", id, username, level, rank);
+            }
+
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+    }
+}
