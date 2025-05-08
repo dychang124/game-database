bf9aaa04ba168298521538077e156843c7dc5d7d
neilchowCS <neilxchow@gmail.com>
2025-05-07 18:54:07 -0700

    connection success

--- backend/application/src/main/java/com/example/App.java
+++ backend/application/src/main/java/com/example/App.java
@@ -1,5 +1,7 @@
 package application.src.main.java.com.example;
-
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
 /**
  * Hello world!
  *
@@ -9,5 +11,17 @@ public class App
     public static void main( String[] args )
     {
         System.out.println( "Hello World!" );
+
+        String url = "jdbc:mysql://localhost:3306/gamedb"; // DB name here
+        String user = "user"; // your username
+        String password = "password"; // your password
+
+        try (Connection conn = DriverManager.getConnection(url, user, password)) {
+            System.out.println("Connected to MySQL database!");
+        } catch (SQLException e) {
+            System.out.println("Connection failed:");
+            e.printStackTrace();
+        }
     }
+
 }
